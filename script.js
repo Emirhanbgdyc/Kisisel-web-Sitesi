84af1b514be557d2453d7d4e2682a981bbeabca7
Emirhan Buğdaycı <148485417+Emirhanbgdyc@users.noreply.github.com>
2025-02-08 00:10:35 +0300

    JavaScript ekleniyor

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ const product = {
     "ALL": [
         { image: "resim/Sandbox.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "Bootstrap", title: "Sandbox", date: "2024-Mayıs", link: "https://sandboxornekproje.netlify.app", projelers: "Sandbox, bootstrap ile oluşturmuş olduğum ilk mobile duyarlı projemdir." },
         { image: "resim/Coffe.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "JavaScript", title: "Coffee", date: "2024-Subat", link: "https://coffeew.netlify.app", projelers: "Coffee,Html Css ve JavaScript ile yapmış olduğum bir projedir." },
-        { image: "resim/Hisar.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "JavaScript", title: "Hisar", date: "2024-Subat", link: "https://coffeew.netlify.app", projelers: "Hisar,yazılama başladığım zaman oluşturduğum bir projedir." },
-        { image: "resim/Landpick.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "JavaScript", title: "Landpick", date: "2024-Temmuz", link: "https://coffeew.netlify.app", projelers: "Landpick,stajımda bana verilen ilk projedir." },
+        { image: "resim/Hisar.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "JavaScript", title: "Hisar", date: "2024-Subat", link: "https://hisarpastanesi.netlify.app", projelers: "Hisar,yazılama başladığım zaman oluşturduğum bir projedir." },
+        { image: "resim/Landpick.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "JavaScript", title: "Landpick", date: "2024-Temmuz", link: "https://daynexbootstrap-2.netlify.app", projelers: "Landpick,stajımda bana verilen ilk projedir." },
         { image: "resim/Portofolio.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "", title: "Portfolio", date: "2023-Aralık", link: "https://coffeew.netlify.app", projelers: "Portfolio,stajımda bana verilen ilk projedir." },
-        { image: "resim/KoruOtel.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "", title: "Koru Otel", date: "2024-Temmuz", link: "https://coffeew.netlify.app", projelers: "Koru Otel,stajımda bana verilen ilk projedir." },
+        { image: "resim/KoruOtel.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "", title: "Koru Otel", date: "2024-Temmuz", link: "http://koruotellprojesi.netlify.app", projelers: "Koru Otel,stajımda bana verilen ilk projedir." },
     ],
     "WEBSITE": [
         { image: "resim/Sandbox.png", tagsp1: "HTML", tagsp2: "CSS", tagsp3: "Bootstrap", title: "Sandbox", date: "2024-Mayıs", projelers: "Sandbox, bootstrap ile oluşturmuş olduğum ilk mobile duyarlı projemdir." },
